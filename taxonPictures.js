1c75f18aa0839f87541546cf09e566172f957b76
Wouter <wouter.koch@artsdatabanken.no>
2020-08-05 16:28:00 +0200

    Add species pics

--- taxonPictures.js
+++ taxonPictures.js
@@ -2,5 +2,12 @@ module.exports = {
   media: {
     "Anas platyrhynchos": 299673,
     "Corvus monedula": 299672,
+    "Gazoryctra ganna": 299684,
+    "Gazoryctra uralensis": 299685,
+    "Hepialus humuli": 299690,
+    "Korscheltellus fusconebulosa": 299689,
+    "Korscheltellus lupulina": 299688,
+    "Phymatopus hecta": 299686,
+    "Triodia sylvina": 299687,
   },
 };
