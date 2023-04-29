7747b1ef072d33bab8202b61b42c7afe567408a1
Wouter Koch <wouter.koch@artsdatabanken.no>
2023-04-30 01:02:00 +0200

    Add species picture albums

--- server.js
+++ server.js
@@ -283,7 +283,7 @@ let simplifyJson = (json) => {
 };
 
 let refreshtaxonimages = async () => {
-  const pages = [342550, 342548]
+  const pages = [342548, 342550, 342551, 342552, 342553]
   let taxa = {}
 
   for (let index = 0; index < pages.length; index++) {
